cdeacbdd290f549242210bf0024d7ff3af7be54a
Dipankar Biswas <84431857+themedesigner3@users.noreply.github.com>
2023-06-03 10:49:38 +0600

    SZDfgsdf

---
+++ app/Http/Controllers/Frontend/CustomerController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers\Frontend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\customer;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class CustomerController extends Controller
+{
+    public function customerSettings(){
+
+        return view("frontend.pages.customerSettings");
+        // if (Auth::guard("customer")->check()) {
+        //     return redirect("/dashboard");
+        // }else{
+        //      return view("frontend.pages.customerSettings");
+        // }
+    }
+
+
+    public function customerUpdate(Request $req){
+
+        // if($req->method("POST")){
+
+            $errors = Validator::make($req->all(),[
+                "name" => "required",
+                "mailPhone" => "required",
+                "nid" => "required",
+                "address" => "required",
+                // "password" => "required|min:8",
+                // "repassword" => "required|same:password",
+            ]);
+
+            if ($errors->fails()) {
+                return response()->json([
+                    "status" => false,
+                    "message" => "error",
+                    "data" => $errors->errors()
+                ]);
+            }
+
+            if(is_numeric($req->mailPhone)){
+
+                $errors = Validator::make($req->all(),[
+                    "mailPhone" => "min:11",
+                ],[
+                    "mailPhone.min" => "Please valid phone number."
+                ]);
+
+            }else{
+                $errors = Validator::make($req->all(),[
+                    "mailPhone" => "email",
+                ],[
+                    "mailPhone.email" => "Please valid email address.",
+                    "mailPhone.customers" => "Email address already exists."
+                ]);
+            }
+
+            if ($errors->fails()) {
+                return response()->json([
+                    "status" => false,
+                    "message" => "error",
+                    "data" => $errors->errors()
+                ]);
+            }
+
+            if($req->image){
+                $path = $req->image;
+                $paths = substr(md5(time()), 0, 10).".".$path->getClientOriginalExtension();
+                $path->move(public_path("customerImage"),$paths);
+                $path_url = 'customerImage/'.$paths;
+                @unlink($path);
+            }else {
+                $path_url = NULL;
+            }
+
+            $customer = customer::where('id',Auth::guard("customer")->user()->id)->first();
+            $customer->name = $req->name;
+            $customer->mailPhone = $req->mailPhone;
+            $customer->nid = $req->nid;
+            $customer->address = $req->address;
+            $customer->image = $path_url;
+            $customer->save();
+
+            if($customer){
+                return redirect()->back()->with([
+                    "status" => true,
+                    "customerReglogin" => true,
+                    "message" => "Customer Profile updated!"
+                ]);
+            }else{
+                return redirect()->back()->with([
+                    "status" => false,
+                    "customerReglogin" => false,
+                    "message" => "Customer Profile not updated!"
+                ]);
+            }
+        // }
+
+    }
+
+    public function customerPasswordChange(Request $req){
+        $this->validate($req, [
+            "oldpassword" => "required",
+            "password" => "required|min:8",
+            "repassword" => "required|same:password",
+        ]);
+
+        $check_data = customer::where('id',Auth::guard("customer")->user()->id)->first();
+
+        if (Hash::check($req->oldpassword, $check_data->password)) {
+            $customer = customer::where('id',Auth::guard("customer")->user()->id)->first();
+            $customer->password = $req->password;
+            $customer->save();
+            if($customer){
+                return redirect()->back()->with([
+                    "status" => true,
+                    "customerReglogin" => true,
+                    "message" => 'Password Change Successfully'
+                ]);
+            }else{
+                return redirect()->back()->with([
+                    "status" => false,
+                    "customerReglogin" => false,
+                    "message" => "Password not change!"
+                ]);
+            }
+        }else{
+            return redirect()->back()->with([
+                "status" => false,
+                "customerReglogin" => false,
+                "message" => "Old password not match!"
+            ]);
+        }
+    }
+}
